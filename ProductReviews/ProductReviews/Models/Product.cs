4118b573811756a307cf08468881066b22ba8a5f
Alex_Fallen_Angel <AlexMrHam@ya.ru>
2016-11-13 17:31:39 +0300

    end

---
+++ ProductReviews/ProductReviews/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProductReviews.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Category { get; set; }
+
+        public int Price { get; set; }
+
+        //Navigation Property
+        public ICollection<Review> Reviews { get; set; }
+    }
+}
